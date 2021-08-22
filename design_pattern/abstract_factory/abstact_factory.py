ddca41277f71a0222be6534d54f1c803d7815489
Hessen <258414420@qq.com>
2021-08-23 00:04:57 +0800

    init repo

---
+++ design_pattern/abstract_factory/abstact_factory.py
@@ -0,0 +1,42 @@
+
+from __future__ import annotations
+from abc import ABC, abstractmethod
+
+
+class AbstractFactory(ABC):
+    """
+    The Abstract Factory interface declares a set of methods that return
+    different abstract products. These products are called a family and are
+    related by a high-level theme or concept. Products of one family are usually
+    able to collaborate among themselves. A family of products may have several
+    variants, but the products of one variant are incompatible with products of
+    another.
+    """
+    @abstractmethod
+    def create_product_a(self) -> AbstractProductA:
+        pass
+
+    @abstractmethod
+    def create_product_b(self) -> AbstractProductB:
+        pass
+
+
+class AbstractProductA(ABC):
+    """
+    Each distinct product of a product family should have a base interface. All
+    variants of the product must implement this interface.
+    """
+
+    @abstractmethod
+    def useful_function_a(self) -> str:
+        pass
+
+
+class AbstractProductB(ABC):
+    """
+    interface of B
+    """
+
+    @abstractmethod
+    def useful_function_b(self) -> str:
+        pass
---
+++ main.py
@@ -0,0 +1,7 @@
+# design pattern demo
+# Press the green button in the gutter to run the script.
+
+if __name__ == '__main__':
+    print("hello desing pattern")
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
